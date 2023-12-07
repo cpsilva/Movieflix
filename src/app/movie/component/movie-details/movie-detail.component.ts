d754e0236d0d242900573daf0fdc05742b8cade0
Caio Silva <caio.silva@threeo.com.br>
2023-12-07 00:01:24 +0000

    Added option to add/remove from watchlist when acessing the movie details page

--- src/app/movie/component/movie-details/movie-detail.component.ts
+++ src/app/movie/component/movie-details/movie-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MovieModel } from '../../models/movie.model';
 import { MovieService } from '../../service/movie.service';
 import { ActivatedRoute } from '@angular/router';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { Location } from '@angular/common';
 
 @Component({
@@ -13,6 +13,9 @@ import { Location } from '@angular/common';
 export class MovieDetailComponent implements OnInit {
 
   public movie!: MovieModel | undefined;
+  public watchList: Array<MovieModel> = new Array<MovieModel>();
+  public itIsOnWatchList: boolean = false;
+  public trailerLink!: SafeResourceUrl;
 
   constructor(private movieService: MovieService,
     private activatedRoute: ActivatedRoute,
@@ -25,6 +28,12 @@ export class MovieDetailComponent implements OnInit {
 
     this.movieService.getMovieById(id).subscribe((result) => {
       this.movie = result;
+      this.trailerLink = this.getTrailerEmbededLink(result!.trailerLink)
+      if (localStorage.getItem("watchList")) {
+        const watchListStorage: Array<MovieModel> = JSON.parse(localStorage.getItem("watchList")!);
+        this.watchList = watchListStorage;
+        this.itIsOnWatchList = this.watchList.some(x => x.id === this.movie!.id)
+      }
     });
   }
 
@@ -36,5 +45,23 @@ export class MovieDetailComponent implements OnInit {
 
   back(): void {
     this._location.back();
+  }
+
+  addToWatchList(movie: MovieModel) {
+    if (this.watchList.find(x => x.id === movie.id)) {
+      return;
+    }
+
+    this.watchList.push(movie);
+    localStorage.setItem("watchList", JSON.stringify(this.watchList));
+    this.itIsOnWatchList = true;
+  }
+
+  removeFromWatchList(movie: MovieModel) {
+    if (this.watchList.find(x => x.id === movie.id)) {
+      this.watchList = this.watchList.filter(x => x.id != movie.id);
+      localStorage.setItem("watchList", JSON.stringify(this.watchList));
+      this.itIsOnWatchList = false;
+    }
   }
 }
